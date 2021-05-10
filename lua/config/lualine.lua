728fe14870e9350347aa83782ec50ad926895ea1
agarof <alban.garofoli@epitech.eu>
2021-05-10 22:38:48 +0200

    Put file path at the center of the status line

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -31,14 +31,33 @@ return function ()
     symbols = { error = ' ', warn = ' ', info = ' ' },
   }
 
+  local spacer = {
+    function () return '%=' end,
+    separator = ''
+  }
+
+  local filename = {
+    'filename',
+    full_path = true,
+    shorten = true,
+  }
+
   require('lualine').setup {
     sections = {
       lualine_a = { 'mode' },
       lualine_b = { 'branch', diagnostics },
-      lualine_c = { 'filename' },
+      lualine_c = { spacer, filename },
       lualine_x = { 'encoding', 'fileformat' },
       lualine_y = { 'filetype' },
       lualine_z = { 'location' }
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { spacer, filename },
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {},
     },
 
     options = {
