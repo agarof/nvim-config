b0bcd1ab874b65da7ae0d21eb1a13d3d2ceae75f
agarof <alban.garofoli@epitech.eu>
2021-06-06 23:31:56 +0200

    Allow lsp servers to have their own on_attach function

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -5,24 +5,30 @@ return function ()
 
   local capabilities = vim.lsp.protocol.make_client_capabilities()
 
-  local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...) utils.buf_map(bufnr, ...) end
+  local make_on_attach = function (server_on_attach)
+    return function (client, bufnr)
+      local function buf_set_keymap(...) utils.buf_map(bufnr, ...) end
 
-    buf_set_keymap('n', 'gd',        '<cmd>lua vim.lsp.buf.declaration()<CR>')
-    buf_set_keymap('n', 'gi',        '<cmd>lua vim.lsp.buf.implementation()<CR>')
-    buf_set_keymap('n', 'K',         '<cmd>lua vim.lsp.buf.hover()<CR>')
-    buf_set_keymap('n', '<C-k>',     '<cmd>lua vim.lsp.buf.signature_help()<CR>')
-    buf_set_keymap('n', '1gD',       '<cmd>lua vim.lsp.buf.type_definition()<CR>')
-    buf_set_keymap('n', 'gr',        '<cmd>lua vim.lsp.buf.references()<CR>')
-    buf_set_keymap('n', 'g0',        '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
-    buf_set_keymap('n', 'gW',        '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
-    buf_set_keymap('n', 'gk',        '<cmd>lua vim.lsp.buf.code_action()<CR>')
-    buf_set_keymap('n', '<leader>d', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>')
-    buf_set_keymap('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>')
+      buf_set_keymap('n', 'gd',        '<cmd>lua vim.lsp.buf.declaration()<CR>')
+      buf_set_keymap('n', 'gi',        '<cmd>lua vim.lsp.buf.implementation()<CR>')
+      buf_set_keymap('n', 'K',         '<cmd>lua vim.lsp.buf.hover()<CR>')
+      buf_set_keymap('n', '<C-k>',     '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+      buf_set_keymap('n', '1gD',       '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+      buf_set_keymap('n', 'gr',        '<cmd>lua vim.lsp.buf.references()<CR>')
+      buf_set_keymap('n', 'g0',        '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+      buf_set_keymap('n', 'gW',        '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
+      buf_set_keymap('n', 'gk',        '<cmd>lua vim.lsp.buf.code_action()<CR>')
+      buf_set_keymap('n', '<leader>d', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>')
+      buf_set_keymap('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>')
 
-    if client.resolved_capabilities.document_formatting then
-      buf_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>")
-      vim.cmd 'autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()'
+      if client.resolved_capabilities.document_formatting then
+        buf_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>")
+        vim.cmd('autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()')
+      end
+
+      if server_on_attach then
+        server_on_attach(client, bufnr)
+      end
     end
   end
 
@@ -51,7 +57,7 @@ return function ()
     }
   )
 
-  vim.lsp.handlers['textDocument/codeAction'] = require'lsputil.codeAction'.code_action_handler
+  vim.lsp.handlers['textDocument/codeAction'] = require('lsputil.codeAction').code_action_handler
 
   vim.fn.sign_define('LspDiagnosticsSignError',
     { text = '', texthl = 'LspDiagnosticsSignError' })
@@ -62,10 +68,9 @@ return function ()
   vim.fn.sign_define('LspDiagnosticsSignHint',
     { text = '', texthl = 'LspDiagnosticsSignHint' })
 
-
   for _, server in ipairs(servers) do
     lsp[server.name].setup {
-      on_attach = on_attach,
+      on_attach = make_on_attach(server.on_attach),
       cmd = server.cmd,
       settings = server.settings,
       capabilities = capabilities
