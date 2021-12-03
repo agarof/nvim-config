2aef91593469eab34d7fce6a04987283b533dc73
agarof <alban.garofoli@epitech.eu>
2021-12-02 19:02:29 -0500

    Update lsp config to neovim 0.6

--- lua/plugins/lspconfig/init.lua
+++ lua/plugins/lspconfig/init.lua
@@ -21,7 +21,7 @@ return {
       buf_set_keymap('n', 'g0',        '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
       buf_set_keymap('n', 'gW',        '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
       buf_set_keymap('n', 'gk',        '<cmd>lua vim.lsp.buf.code_action()<CR>')
-      buf_set_keymap('n', '<leader>d', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>')
+      buf_set_keymap('n', '<leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>')
       buf_set_keymap('n', '<leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>')
 
       if client.resolved_capabilities.document_formatting then
@@ -71,14 +71,10 @@ return {
 
     vim.lsp.handlers['textDocument/codeAction'] = require('lsputil.codeAction').code_action_handler
 
-    vim.fn.sign_define('LspDiagnosticsSignError',
-    { text = '', texthl = 'LspDiagnosticsSignError' })
-    vim.fn.sign_define('LspDiagnosticsSignWarning',
-    { text = '', texthl = 'LspDiagnosticsSignWarning' })
-    vim.fn.sign_define('LspDiagnosticsSignInformation',
-    { text = '', texthl = 'LspDiagnosticsSignInformation' })
-    vim.fn.sign_define('LspDiagnosticsSignHint',
-    { text = '', texthl = 'LspDiagnosticsSignHint' })
+    vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
+    vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
+    vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
+    vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
 
     for _, server in ipairs(servers) do
       lsp[server.name].setup {
